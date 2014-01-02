f1bb70d49592d8a1d84200316da8b0b847452463
Joe Nelson <cred+github@begriffs.com>
2014-01-01 16:23:00 -0800

    More verbose installer

--- install.sh
+++ install.sh
@@ -1,38 +1,52 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+msg() { echo "--- $@" 1>&2; }
+detail() { echo "	$@" 1>&2; }
+
+for i in git make vim; do
+  command -v $i >/dev/null
+  if [ $? -ne 0 ] ; then
+    msg "Installer requires ${i}. Please install $i and try again."
+    exit 1
+  fi
+done
 
 endpath="$HOME/.begriffs-vim"
 
 if [ ! -e $endpath/.git ]; then
-    echo "Cloning begriffs/dotfiles"
-    git clone https://github.com/begriffs/dotfiles.git $endpath
+  msg "Cloning begriffs/dotfiles"
+  git clone https://github.com/begriffs/dotfiles.git $endpath
 else
-    echo "Updating begriffs/dotfiles"
-    cd $endpath && git pull
+  msg "Existing installation detected"
+  msg "Updating from begriffs/dotfiles"
+  cd $endpath && git pull
 fi
 
 if [ -e ~/.vim/colors ]; then
-  echo "Preserving color scheme files"
+  msg "Preserving color scheme files"
   cp -R ~/.vim/colors $endpath/colors
 fi
 
-echo "backing up current vim config"
 today=`date +%Y%m%d_%H%M%S`
-for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today; done
+msg "Backing up current vim config"
+for i in $HOME/.vim $HOME/.vimrc $HOME/.gvimrc; do [ -e $i ] && [ ! -L $i ] && mv $i $i.$today && detail "$i.$today"; done
 
-echo "Creating symlinks"
+msg "Creating symlinks"
+detail "~/.vimrc -> $endpath/.vimrc"
+detail "~/.vim   -> $endpath/.vim"
 ln -sf $endpath/.vimrc $HOME/.vimrc
 if [ ! -d $endpath/.vim/bundle ]; then
-    mkdir -p $endpath/.vim/bundle
+  mkdir -p $endpath/.vim/bundle
 fi
 ln -sf $endpath/.vim $HOME/.vim
 
 if [ ! -e $HOME/.vim/bundle/vundle ]; then
-    echo "Installing Vundle"
-    git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+  msg "Installing Vundle"
+  git clone http://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
 fi
 
-echo "Installing plugins using Vundle"
+msg "Installing plugins using Vundle"
 system_shell=$SHELL
 export SHELL="/bin/sh"
-vim -u $endpath/.vimrc +BundleInstall! +BundleClean +qall
+vim -u $endpath/.vimrc +BundleInstall! +BundleClean! +qall
 export SHELL=$system_shell
